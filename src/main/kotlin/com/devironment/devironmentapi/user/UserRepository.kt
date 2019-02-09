b4f9e735fcdf917a4a3cd8aa8cf5b9c7d4c304fe
Manu Gopinathan <manu.gopinathan@knowit.no>
2019-02-09 14:13:26 +0100

    Minor fixes: sql

--- src/main/kotlin/com/devironment/devironmentapi/user/UserRepository.kt
+++ src/main/kotlin/com/devironment/devironmentapi/user/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.devironment.devironmentapi.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, String> {
 
+    @Query("select u from User u where userId = :userId")
     fun findByUserId(userId: String): User?
 }
